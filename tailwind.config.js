40313a0139a85831e546e0b6d27c144a8f642355
Ryan Deshon <ryandeshon@gmail.com>
2023-06-25 18:09:45 -0400

    Clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./assets/**/*.css",
-    "./components/*.{vue,js}",
-    "./components/**/*.{vue,js}",
-    "./pages/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./*.{vue,js,ts}",
-    "./nuxt.config.{js,ts}",
+    './assets/**/*.css',
+    './components/*.{vue,js}',
+    './components/**/*.{vue,js}',
+    './pages/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './*.{vue,js,ts}',
+    './nuxt.config.{js,ts}'
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem'
+      }
+    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
-
